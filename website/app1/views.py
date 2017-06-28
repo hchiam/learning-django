9caeebd616443b9dcab52fffe93afcbf598301d9
hchiam <hchiam@gmail.com>
2017-06-28 13:19:40 -0400

    show progression

--- website/app1/views.py
+++ website/app1/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # from django.template import loader # to load separate template files
-from django.shortcuts import render # instead of using loader
+from django.shortcuts import render # instead of using loader or HttpResponse
 # from django.http import Http404
 from django.shortcuts import get_object_or_404 # instead of using Http404
 from .models import Album
@@ -14,10 +14,18 @@ def index(request):
     #     html += '<a href="' + url + '">' + album.album_title + '</a><br/>'
     # return HttpResponse(html)
     
+    # |
+    # |
+    # V
+    
     # all_albums = Album.objects.all() # get database data
     # template = loader.get_template('app1/index.html') # by default django looks into local /templates/ folder
     # context = {'all_albums': all_albums}
     # return HttpResponse(template.render(context, request))
+    
+    # |
+    # |
+    # V
     
     all_albums = Album.objects.all() # get database data
     template = 'app1/index.html'
@@ -28,11 +36,19 @@ def index(request):
 def detail(request, album_id): # example: album_id=712
     # return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
     
+    # |
+    # |
+    # V
+    
     # # responding depending on if request gets a valid entry
     # try:
     #     album = Album.objects.get(id=album_id)
     # except Album.DoesNotExist:
     #     raise Http404('Album does not exist. :(')
+    
+    # |
+    # |
+    # V
     
     album = get_object_or_404(Album, pk=album_id) # to replace that try/except statement
     
